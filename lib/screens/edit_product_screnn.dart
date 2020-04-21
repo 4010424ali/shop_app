57f8fad7f8f3fa354a6d990911310ddb450b2bdb
Ali Hamza <ali.hamza4010424@gmail.com>
2020-04-21 12:12:01 +0500

    more about Form in flutter

--- lib/screens/edit_product_screnn.dart
+++ lib/screens/edit_product_screnn.dart
@@ -8,6 +8,15 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
+  final _descriptionNode = FocusNode();
+
+  // We should close the focus node. Otherwise it will leak with the momery
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _descriptionNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +41,16 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.number,
                 focusNode: _priceFocusNode,
-              )
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Description'),
+                maxLines: 3,
+                keyboardType: TextInputType.multiline,
+                focusNode: _descriptionNode,
+              ),
             ],
           ),
         ),
